945a50329ba9f1dd009b57b0c183b0b3917871c6
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-01 19:41:11 -0300

    Adiciona black e formata arquivos.

--- db.py
+++ db.py
@@ -1,12 +1,16 @@
 from peewee import *
 from playhouse.mysql_ext import MySQLConnectorDatabase
-from typing import  Dict, Any, List
+from typing import Dict, Any, List
 
-db = MySQLConnectorDatabase('biblioteca_manager', host='localhost', user='root', password='')
+db = MySQLConnectorDatabase(
+    "biblioteca_manager", host="localhost", user="root", password=""
+)
+
 
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class Livros(BaseModel):
     titulo = CharField(null=False, unique=True)
@@ -14,29 +18,37 @@ class Livros(BaseModel):
     foto = CharField(null=False)
     autor = CharField(null=False)
 
+
 class RepositorioLivros:
     # XXX Na segunda vez que rodamos a API isso quebra.
     def __init__(self):
         db.create_tables([Livros])
 
-    def insere_livro(self, livro: Dict[str, Any])->None:
-        Livros.create(titulo=livro['titulo'], editora=livro['editora'], foto=livro['foto'], autor=livro['autor'])
-    
-    def livros_cadastrados(self)->List[Livros]:
+    def insere_livro(self, livro: Dict[str, Any]) -> None:
+        Livros.create(
+            titulo=livro["titulo"],
+            editora=livro["editora"],
+            foto=livro["foto"],
+            autor=livro["autor"],
+        )
+
+    def livros_cadastrados(self) -> List[Livros]:
         livros = []
         for livro in Livros.select():
-            livros.append({
-            'titulo': livro.titulo,
-            'editora': livro.editora,
-            'foto': livro.foto,
-            'autor': livro.autor,
-            'id': livro.id
-            })
+            livros.append(
+                {
+                    "titulo": livro.titulo,
+                    "editora": livro.editora,
+                    "foto": livro.foto,
+                    "autor": livro.autor,
+                    "id": livro.id,
+                }
+            )
         return livros
 
-    def deleta_livro(self, id_livro: int)->None:
+    def deleta_livro(self, id_livro: int) -> None:
         Livros.delete_by_id(id_livro)
         db.close()
 
-    def atualiza_livro(self,id_livro, livro)->None:
+    def atualiza_livro(self, id_livro, livro) -> None:
         Livros.update(livro).where("id" == id_livro).execute()
--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from models import ModeloDoItem, ModeloDoItemResposta
 pw = RepositorioLivros()
 app = FastAPI()
 
+
 @app.get("/livros")
 def index():
     return pw.livros_cadastrados()
+
 
 @app.post("/livros/", response_model=ModeloDoItemResposta, status_code=201)
 def cadastra_livro(item_a_inserir: ModeloDoItem):
     return pw.insere_livro(item_a_inserir.dict())
 
+
 @app.put("/livros/{id_do_livro}", response_model=ModeloDoItemResposta)
 def atualiza_livro(id_do_livro, obra: ModeloDoItem):
     return pw.atualiza_livro(id_do_livro, obra.dict())
+
 
 @app.delete("/livros/{id_do_livro}", response_model=ModeloDoItemResposta)
 def deleta_livro(id_do_livro):
