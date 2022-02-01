1ad0a0b1a3af1f6694ab50235d4dfec8a6192439
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-01 18:56:21 -0300

    Add fixes.

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from peewee import *
 from playhouse.mysql_ext import MySQLConnectorDatabase
-from typing import  Dict, Any
+from typing import  Dict, Any, List
 
 db = MySQLConnectorDatabase('biblioteca_manager', host='localhost', user='root', password='')
 
@@ -15,29 +15,27 @@ class Livros(BaseModel):
     autor = CharField(null=False)
 
 class RepositorioLivros:
-    def __init__(self):
-        db.create_tables([Livros])
+    # XXX Na segunda vez que rodamos a API isso quebra.
+    # def __init__(self):
+    #     db.create_tables([Livros])
 
-    def insere_livro(self, livro: Dict[str, Any]):
+    def insere_livro(self, livro: Dict[str, Any])->None:
         Livros.create(titulo=livro['titulo'], editora=livro['editora'], foto=livro['foto'], autor=livro['autor'])
     
-    def livros_cadastrados(self):
-        lista_de_livros = []
+    def livros_cadastrados(self)->List[Livros]:
+        livros = []
         for livro in Livros.select():
-            lista_de_livros.append({
+            livros.append({
             'titulo': livro.titulo,
             'editora': livro.editora,
             'foto': livro.foto,
             'autor': livro.autor,
             'id': livro.id
             })
-        return lista_de_livros
+        return livros
 
-    def deleta_livro(self, id_livro: int):
+    def deleta_livro(self, id_livro: int)->None:
         Livros.delete_by_id(id_livro)
 
-    def atualiza_livro(self,id_livro, livro):
-        # livro_encontrado = Livros.get_id(id_livro)
-        q = (Livros.update({Livros.titulo: livro['titulo'], Livros.editora: livro['editora'], Livros.foto: livro['foto'], Livros.autor: livro['autor']})
-            .where(Livros.id == id_livro))
-        q.execute()
+    def atualiza_livro(self,id_livro, livro)->None:
+        Livros.update(livro).where("id" == id_livro).execute()
